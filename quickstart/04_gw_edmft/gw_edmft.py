e786a422fb651f4869e2dacd1f29b2759e76df5c
cnyeh <cyeh@flatironinstitute.org>
2026-05-26 17:28:03 -0400

    work on tutorial 04

--- quickstart/04_gw_edmft/gw_edmft.py
+++ quickstart/04_gw_edmft/gw_edmft.py
@@ -14,7 +14,7 @@ E1 = modest.make_embedding(obe.C_space)
 mf_params = {"prefix": "nio", "outdir": "nio_555/out", "nbnd": 40}
 mf = coqui.make_mf(coqui_mpi, params=mf_params, mf_type='qe')
 
-thc_params = {"thresh": 1e-5}
+thc_params = {"thresh": 1e-3}
 thc = coqui.make_thc_coulomb(mf=mf, params=thc_params)
 
 gw_edmft_params = {
