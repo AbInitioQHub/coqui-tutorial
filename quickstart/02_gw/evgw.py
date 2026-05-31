fc984ed66c3bf8972a20ecde62dd1cd805f650c5
cnyeh <cyeh@flatironinstitute.org>
2026-05-31 19:26:44 -0400

    gw python script

---
+++ quickstart/02_gw/evgw.py
@@ -0,0 +1,31 @@
+from mpi4py import MPI
+import coqui
+
+# Create CoQui MPI handler and set logging verbosity in the beginning
+coqui_mpi = coqui.MpiHandler()
+coqui.set_verbosity(coqui_mpi, output_level=1)
+
+# Step 1: Build mean-field object
+mf_params = {
+    "prefix": "si",
+    "outdir": "si_555/out",
+    "nbnd": 20,
+}
+mf = coqui.make_mf(coqui_mpi, params=mf_params, mf_type="qe")
+
+# Step 2: Build THC Coulomb Hamiltonian
+thc_params = {
+    "thresh": 1e-3,
+    "save": "thc.coulomb.h5"
+}
+thc = coqui.make_thc_coulomb(mf=mf, params=thc_params)
+
+# Step 3: Run evGW calculation
+gw_params = {
+    "outdir": "./",
+    "output": "si.evgw",
+    "niter": 6,
+    "beta": 1000, 
+    "iaft": {"prec": "low"}
+}
+coqui.run_evgw(params=gw_params, h_int=thc)
